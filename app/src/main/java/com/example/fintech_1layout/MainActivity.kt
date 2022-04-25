038e44c6d6cdcbba5ec29bd8189eafeb6aa03001
Muhammad Hasyim Asy'ari <hasy1m@MacBook-Air-Muhammad.local>
2022-04-25 21:27:35 +0700

    1st Practicum

---
+++ app/src/main/java/com/example/fintech_1layout/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.fintech_1layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnHome: Button
+    private lateinit var btnProfile: Button
+    private lateinit var btnSetting: Button
+
+    override fun onCreate( savedInstanceState: Bundle? ) {
+        super.onCreate( savedInstanceState )
+        setContentView( R.layout.activity_main )
+
+        btnHome     = findViewById( R.id.btn_home )
+        btnProfile  = findViewById( R.id.btn_profile )
+        btnSetting  = findViewById( R.id.btn_setting )
+    }
+}
